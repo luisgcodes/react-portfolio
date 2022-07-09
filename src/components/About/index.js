a64b9217287179af958633c1b9775c0503741d5c
luisgcodes <lcienfuegos95@gmail.com>
2022-07-09 13:46:39 -0700

    header/nav

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,4 @@
-
+// A little about me!!
 const About = () => {
   return (
     <section className="content" id="about">  
@@ -6,19 +6,12 @@ const About = () => {
       <div className="about-me">
         <div>
           <p>Hello, I'm Luis — An Aspiring Software Engineer who is on the road to specialize in Front-end Development.</p>
-          <p> 
-          Front-end development caught my attention late of 2021 and ever since it has kept it's grip on me. I began to do research and soon decided to go the "Self Taught" route but I knew that if I wanted to get the most out of the early stages of my journey, I should enroll in a bootcamp and that's what I did.
-          </p>
-          {/* <hr /> */}
-          <p>
-          Apart from this new found love, I enjoy gaming and finding new ways to become as creative as I can be with the things I get into. I've dipped my toes in photography, photoshop, video editing, designing and producing shirts/decals.
-          </p>
-
-        </div>
-        
+          <p>Front-end development caught my attention late of 2021 and ever since it has kept it's grip on me. I began to do research and soon decided to go the "Self Taught" route but I knew that if I wanted to get the most out of the early stages of my journey, I should enroll in a bootcamp and that's what I did.</p>
+          {}
+          <p>Apart from this new found love, I enjoy gaming and finding new ways to become as creative as I can be with the things I get into. I've dipped my toes in photography, photoshop, video editing, designing and producing shirts/decals.</p>
+        </div> 
       </div>
     </section>
   )
 }
-
 export default About;
---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import Navigation from "../Navigation";
+
+
+// Header
+
+const Header = (props) => {
+  return (
+    <header>
+      <div className="header-block">
+        <div className="dev-name">Luis Gutierrez</div>
+            <div className="subhead">Front-End Developer</div>
+      </div>
+      <Navigation 
+        currentPage={props.currentPage}
+        handlePageChange={props.handlePageChange}
+      />
+    </header>
+  )
+}
+export default Header;
